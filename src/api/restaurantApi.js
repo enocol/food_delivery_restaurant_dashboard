ba02e85d9edee048bad4bb10cc1b440157f92d6b
enocol <eno_col@yahoo.com>
2026-07-26 19:01:18 +0100

    Prepare code base for deployment

--- src/api/restaurantApi.js
+++ src/api/restaurantApi.js
@@ -1,5 +1,6 @@
-// const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = (
+  import.meta.env.VITE_API_URL || "http://localhost:5000"
+).replace(/\/$/, "");
 
 const TOKEN_KEY = "restaurant_jwt";
 
@@ -15,7 +16,6 @@ function authFetch(url, options = {}) {
       ...options.headers,
     },
   });
-  console.log(token);
 }
 
 /**
